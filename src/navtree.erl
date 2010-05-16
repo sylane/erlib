40e4e5de05ed5a4b5747a90af4caa0ef721eff57
Sebastien Merle <s.merle@gmail.com>
2010-05-16 23:46:18 +0200

    Added a tree structure with O(1) updates of the current node

---
+++ src/navtree.erl
@@ -0,0 +1,175 @@
+%% ===========================================================================
+%% @doc        Tree structure with a cursor pointing to a node, where moving
+%%             the cursor to the current node parent or childs, updating
+%%             current node value and adding child nodes are O(1) operations.
+%% @since      May 15, 2010
+%% @version    1.0
+%% @copyright  (c) 2009, Sebastien Merle <s.merle@gmail.com>
+%% @authors    Sebastien Merle <s.merle@gmail.com>
+%% @end
+%%
+%% Copyright (c) 2009, Sebastien Merle <s.merle@gmail.com>
+%% All rights reserved.
+%%
+%% Redistribution and use in source and binary forms, with or without
+%% modification, are permitted provided that the following conditions are met:
+%%
+%%   * Redistributions of source code must retain the above copyright
+%%     notice, this list of conditions and the following disclaimer.
+%%   * Redistributions in binary form must reproduce the above copyright
+%%     notice, this list of conditions and the following disclaimer in the
+%%     documentation and/or other materials provided with the distribution.
+%%   * Neither the name of "erlib" nor the names of its contributors may be
+%%     used to endorse or promote products derived from this software without
+%%     specific prior written permission.
+%%
+%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+%% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+%% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+%% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+%% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+%% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+%% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+%% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+%% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+%% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+%% POSSIBILITY OF SUCH DAMAGE.
+%% ===========================================================================
+
+-module(navtree).
+
+-author('Sebastien Merle <s.merle@gmail.com>').
+
+%% --------------------------------------------------------------------
+%% Includes
+%% --------------------------------------------------------------------
+
+-include("navtree.hrl").
+
+%% --------------------------------------------------------------------
+%% Exports
+%% --------------------------------------------------------------------
+
+%% API exports
+-export([new/1,
+         is_root/1,
+         is_leaf/1,
+         get_child_count/1,
+         get_path/1,
+         go_path/2,
+         go_top/1,
+         go_up/1,
+         go_down/2,
+         get_current/1,
+         update_current/2,
+         add_child/2]).
+
+%% --------------------------------------------------------------------
+%% Macros
+%% --------------------------------------------------------------------
+
+% Tree's state record name
+-define(St, ?NAVTREE_RECNAME).
+
+
+%% ===================================================================
+%% API Functions
+%% ====================================================================
+
+%% --------------------------------------------------------------------
+%% @doc Creates a new tree with the specified root node.
+-spec new(Root::term()) -> Tree::navtree().
+%% --------------------------------------------------------------------
+new(Root) -> #?St{current = {Root, {}}, indexes = [], branches = []}.
+
+%% --------------------------------------------------------------------
+%% @doc Tells if the cursor is on the tree root.
+-spec is_root(Tree::navtree()) -> IsRoot::bool().
+%% --------------------------------------------------------------------
+is_root(?NAVTREE_MATCH_ROOT()) -> true;
+is_root(_)                     -> false.
+
+%% --------------------------------------------------------------------
+%% @doc Tells if the cursor is on a leaf.
+-spec is_leaf(Tree::navtree()) -> IsLeaf::bool().
+%% --------------------------------------------------------------------
+is_leaf(?NAVTREE_MATCH_LEAF()) -> true;
+is_leaf(_)                     -> false.
+
+%% --------------------------------------------------------------------
+%% @doc Gives the number of child of the current node.
+-spec get_child_count(Tree::navtree()) -> Count::pos_integer().
+%% --------------------------------------------------------------------
+get_child_count(#?St{current = {_, Childs}}) -> size(Childs).
+
+%% --------------------------------------------------------------------
+%% @doc Gives the path to the current node.
+-spec get_path(Tree::navtree()) -> Path::navtree_path().
+%% --------------------------------------------------------------------
+get_path(#?St{indexes = Indexes}) -> lists:reverse(Indexes).
+
+%% --------------------------------------------------------------------
+%% @doc Moves the cursor to the tree root.
+-spec go_top(Tree::navtree()) -> NewTree::navtree().
+%% --------------------------------------------------------------------
+go_top(?NAVTREE_MATCH_ROOT() = Tree) -> Tree;
+go_top(Tree)                         -> go_top(go_up(Tree)).
+
+%% --------------------------------------------------------------------
+%% @doc Moves the cursor to the current node parent.
+-spec go_up(Tree::navtree()) -> NewTree::navtree().
+%% --------------------------------------------------------------------
+go_up(?NAVTREE_MATCH_ROOT()) ->
+    erlang:error(no_parent);
+go_up(Tree) ->
+    #?St{current = Curr, indexes = [I |Is], branches = [{E, C} |Bs]} = Tree,
+    NewChilds = erlang:setelement(I, C, Curr),
+    Tree#?St{current = {E, NewChilds}, indexes = Is, branches = Bs}.
+
+%% --------------------------------------------------------------------
+%% @doc Moves the cursor to one of the current node child.
+-spec go_down(Index::navtree_index(), Tree::navtree()) -> NewTree::navtree().
+%% --------------------------------------------------------------------
+go_down(_Index, ?NAVTREE_MATCH_LEAF()) ->
+    erlang:error(no_child);
+go_down(Index, Tree) ->
+    #?St{current = {_, Childs} = Curr, indexes = Is, branches = Bs} = Tree,
+    NewCurr = element(Index, Childs),
+    Tree#?St{current = NewCurr, indexes = [Index |Is], branches = [Curr |Bs]}.
+
+%% --------------------------------------------------------------------
+%% @doc Moves the cursor to the specified path.
+-spec go_path(Path::navtree_path(), Tree::navtree()) -> NewTree::navtree().
+%% --------------------------------------------------------------------
+go_path(Path, Tree) -> intern_go(go_top(Tree), Path).
+
+%% --------------------------------------------------------------------
+%% @doc Gives the current node value.
+-spec get_current(Tree::navtree()) -> NodeValue::term().
+%% --------------------------------------------------------------------
+get_current(?NAVTREE_MATCH_CURRENT(Curr)) -> Curr.
+
+%% --------------------------------------------------------------------
+%% @doc Updates the current node value.
+-spec update_current(NewNodeValue::term(), Tree::navtree()) ->
+        {OldNodeValue::term(), NewTree::navtree()}.
+%% --------------------------------------------------------------------
+update_current(Node, #?St{current = {OldNode, Childs}} = Tree) ->
+    {OldNode, Tree#?St{current = {Node, Childs}}}.
+
+%% --------------------------------------------------------------------
+%% @doc Adds a child node to the current node.
+-spec add_child(ChildNode::term(), Tree::navtree()) ->
+        {ChildIndex::navtree_index(), NewTree::navtree()}.
+%% --------------------------------------------------------------------
+add_child(Child, #?St{current = {Node, Childs}} = Tree) ->
+    NewChilds = erlang:append_element(Childs, {Child, {}}),
+    {size(NewChilds), Tree#?St{current = {Node, NewChilds}}}.
+
+
+%% ====================================================================
+%% Local Functions
+%% ====================================================================
+
+intern_go(Tree, [])       -> Tree;
+intern_go(Tree, [I |Rem]) -> intern_go(go_down(I, Tree), Rem).
---
+++ test/test_navtree.erl
@@ -0,0 +1,228 @@
+%% ===========================================================================
+%% @doc        Module navtree unit tests.
+%% @since      May 15, 2010
+%% @version    1.0
+%% @copyright  (c) 2009, Sebastien Merle <s.merle@gmail.com>
+%% @authors    Sebastien Merle <s.merle@gmail.com>
+%% @end
+%%
+%% Copyright (c) 2009, Sebastien Merle <s.merle@gmail.com>
+%% All rights reserved.
+%%
+%% Redistribution and use in source and binary forms, with or without
+%% modification, are permitted provided that the following conditions are met:
+%%
+%%   * Redistributions of source code must retain the above copyright
+%%     notice, this list of conditions and the following disclaimer.
+%%   * Redistributions in binary form must reproduce the above copyright
+%%     notice, this list of conditions and the following disclaimer in the
+%%     documentation and/or other materials provided with the distribution.
+%%   * Neither the name of "erlib" nor the names of its contributors may be
+%%     used to endorse or promote products derived from this software without
+%%     specific prior written permission.
+%%
+%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+%% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+%% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+%% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+%% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+%% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+%% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+%% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+%% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+%% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+%% POSSIBILITY OF SUCH DAMAGE.
+%% ===========================================================================
+
+-module(test_navtree).
+
+-author('Sebastien Merle <s.merle@gmail.com>').
+
+%% --------------------------------------------------------------------
+%% Includes
+%% --------------------------------------------------------------------
+
+-include_lib("eunit/include/eunit.hrl").
+
+-include("navtree.hrl").
+
+%% --------------------------------------------------------------------
+%% Imports
+%% --------------------------------------------------------------------
+
+-import(navtree, [new/1,
+                  is_root/1,
+                  is_leaf/1,
+                  get_child_count/1,
+                  get_path/1,
+                  go_path/2,
+                  go_top/1,
+                  go_up/1,
+                  go_down/2,
+                  get_current/1,
+                  update_current/2,
+                  add_child/2]).
+
+%% --------------------------------------------------------------------
+%% Exports
+%% --------------------------------------------------------------------
+
+-export([coverage/0]).
+
+
+%% ====================================================================
+%% Coverage
+%% ====================================================================
+
+coverage() ->
+    Module = navtree,
+    Package = "",
+    RelBeamPath = io_lib:format("ebin/~w.beam", [Module]),
+    RelSourcePath = io_lib:format("~s/src/~w.erl", [Package, Module]),
+    RelReportPath = io_lib:format("~s/test/coverage_~w.html", [Package, Module]),
+    RelPkgIncludePath = io_lib:format("~s/include", [Package]),
+    BeamPath = code:which(Module),
+    RootPath = re:replace(BeamPath, RelBeamPath, "", [{return, list}]),
+    SourcePath = RootPath ++ RelSourcePath,
+    ReportPath = RootPath ++ RelReportPath,
+    IncludePath = RootPath ++ RelPkgIncludePath,
+    {ok, Module} = cover:compile(SourcePath, [{i, IncludePath}]),
+    ?MODULE:test(),
+    cover:analyse_to_file(Module, ReportPath, [html]),
+    code:purge(Module),
+    code:delete(Module),
+    ok.
+
+
+%% ====================================================================
+%% Unit Tests
+%% ====================================================================
+
+basic_test() ->
+    test_list(new(root),
+              [                     mk_check_full(root, true, true, [], 0),
+               mk_add_child(a1, 1), mk_check_full(root, true, false, [], 1),
+               mk_add_child(a2, 2), mk_check_full(root, true, false, [], 2),
+               mk_add_child(a3, 3), mk_check_full(root, true, false, [], 3),
+               mk_go_down(2),       mk_check_full(a2, false, true, [2], 0),
+               mk_add_child(b1, 1), mk_check_full(a2, false, false, [2], 1),
+               mk_add_child(b2, 2), mk_check_full(a2, false, false, [2], 2),
+               mk_go_down(1),       mk_check_full(b1, false, true, [2, 1], 0),
+               mk_add_child(c1, 1), mk_check_full(b1, false, false, [2, 1], 1),
+               mk_add_child(c2, 2), mk_check_full(b1, false, false, [2, 1], 2),
+               mk_add_child(c3, 3), mk_check_full(b1, false, false, [2, 1], 3),
+               mk_go_down(3),       mk_check(c3, [2, 1, 3], 0),
+               mk_update(c3u, c3),  mk_check(c3u, [2, 1, 3], 0),
+               mk_go_up(),          mk_check(b1, [2, 1], 3),
+               mk_update(b1u, b1),  mk_check(b1u, [2, 1], 3),
+               mk_go_down(2),       mk_check(c2, [2, 1, 2], 0),
+               mk_go_up(),          mk_check(b1u, [2, 1], 3),
+               mk_go_down(3),       mk_check(c3u, [2, 1, 3], 0),
+               mk_go_top(),         mk_check(root, [], 3),
+               mk_go_down(3),       mk_check(a3, [3], 0),
+               mk_add_child(d1, 1), mk_check(a3, [3], 1),
+               mk_add_child(d2, 2), mk_check(a3, [3], 2),
+               mk_add_child(d3, 3), mk_check(a3, [3], 3),
+               mk_go_down(1),       mk_check(d1, [3, 1], 0),
+               mk_add_child(e1, 1), mk_check(d1, [3, 1], 1),
+               mk_go_down(1),       mk_check(e1, [3, 1, 1], 0),
+               mk_add_child(f1, 1), mk_check(e1, [3, 1, 1], 1),
+               mk_add_child(f2, 2), mk_check(e1, [3, 1, 1], 2),
+               mk_go_down(2),       mk_check(f2, [3, 1, 1, 2], 0),
+               mk_add_child(g1, 1), mk_check(f2, [3, 1, 1, 2], 1),
+               mk_go_down(1),       mk_check(g1, [3, 1, 1, 2, 1], 0),
+               mk_update(g1u, g1),  mk_check(g1u, [3, 1, 1, 2, 1], 0),
+               mk_go_up(),          mk_check(f2, [3, 1, 1, 2], 1),
+               mk_go_up(),          mk_check(e1, [3, 1, 1], 2),
+               mk_go_down(1),       mk_check(f1, [3, 1, 1, 1], 0),
+               mk_update(f1u, f1),  mk_check(f1u, [3, 1, 1, 1], 0),
+               mk_go_up(),          mk_check(e1, [3, 1, 1], 2),
+               mk_go_up(),          mk_check(d1, [3, 1], 1),
+               mk_add_child(e2, 2), mk_check(d1, [3, 1], 2),
+               mk_go_up(),          mk_check(a3, [3], 3),
+               mk_update(a3u, a3),  mk_check(a3u, [3], 3),
+               mk_go_top(),         mk_check(root, [], 3),
+               mk_update(r, root),  mk_check(r, [], 3),
+               mk_go_path([2, 1, 3]),        mk_check(c3u, [2, 1, 3], 0),
+               mk_go_path([3, 1, 1, 2, 1]),  mk_check(g1u, [3, 1, 1, 2, 1], 0),
+               mk_go_path([2, 1]),           mk_check(b1u, [2, 1], 3),
+               mk_go_path([3, 1, 1, 1]),     mk_check(f1u, [3, 1, 1, 1], 0),
+               mk_go_path([3]),              mk_check(a3u, [3], 3),
+               mk_go_path([]),               mk_check(r, [], 3),
+               mk_go_path([3, 1, 2]),        mk_check(e2, [3, 1, 2], 0),
+               mk_add_child(h1, 1), mk_check(e2, [3, 1, 2], 1),
+               mk_add_child(h2, 2), mk_check(e2, [3, 1, 2], 2),
+               mk_go_path([1]),              mk_check(a1, [1], 0),
+               mk_update(a1u, a1),  mk_check(a1u, [1], 0),
+               mk_go_path([3, 1, 2, 2]),     mk_check(h2, [3, 1, 2, 2], 0)
+               ]).
+
+
+%% ====================================================================
+%% Internal Functions
+%% ====================================================================
+
+mk_check_full(ExpChild, ExpIsRoot, ExpIsLeaf, ExpPath, ExpChildCount) ->
+    fun(T) ->
+            case ExpIsRoot of
+                true ->
+                    ?assertMatch(?NAVTREE_MATCH_ROOT(), T),
+                    ?assertEqual(true, is_root(T));
+                false ->
+                    ?assertNot(?MATCHES(?NAVTREE_MATCH_ROOT(), T)),
+                    ?assertEqual(false, is_root(T))
+            end,
+            case ExpIsLeaf of
+                true ->
+                    ?assertMatch(?NAVTREE_MATCH_LEAF(), T),
+                    ?assertEqual(true, is_leaf(T));
+                false ->
+                    ?assertNot(?MATCHES(?NAVTREE_MATCH_LEAF(), T)),
+                    ?assertEqual(false, is_leaf(T))
+            end,
+            ?assertMatch(?NAVTREE_MATCH_CURRENT(ExpChild), T),
+            ?assertEqual(ExpChild, get_current(T)),
+            ?assertEqual(ExpChildCount, get_child_count(T)),
+            ?assertEqual(ExpPath, get_path(T)),
+            T
+    end.
+
+mk_check(ExpChild, ExpPath, ExpChildCount) ->
+    fun(T) ->
+            ?assertMatch(?NAVTREE_MATCH_CURRENT(ExpChild), T),
+            ?assertEqual(ExpChild, get_current(T)),
+            ?assertEqual(ExpChildCount, get_child_count(T)),
+            ?assertEqual(ExpPath, get_path(T)),
+            T
+    end.
+
+mk_add_child(Child, ExpChildIdx) ->
+    fun(T0) ->
+            Result = add_child(Child, T0),
+            ?assertMatch({ExpChildIdx, _}, Result),
+            {_, T} = Result,
+            T
+    end.
+
+mk_go_down(Index) ->
+    fun(T) -> go_down(Index, T) end.
+
+mk_go_up() ->
+    fun(T) -> go_up(T) end.
+
+mk_go_top() ->
+    fun(T) -> go_top(T) end.
+
+mk_go_path(Path) ->
+    fun(T) -> go_path(Path, T) end.
+
+mk_update(Value, Expected) ->
+    fun(T) ->
+            Result = update_current(Value, T),
+            ?assertMatch({Expected, _}, Result),
+            {_, NewT} = Result,
+            NewT
+    end.
+
+test_list(_Tree, []) -> ok;
+test_list(Tree, [Fun |Ops]) -> test_list(Fun(Tree), Ops).
